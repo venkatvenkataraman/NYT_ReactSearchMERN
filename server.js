b690908a048e7a8ba17db1ed6146c472ff03ab87
venkatvenkataraman <venkat1010@gmail.com>
2018-03-29 19:48:32 -0500

    after yarn build command

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
